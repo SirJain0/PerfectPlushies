00463eb3eea4c3384e2d2f7f80617e65bbabb186
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-02-28 21:09:34 -0500

    Set up environment

---
+++ src/main/java/sirjain/PerfectPlushiesMain.java
@@ -0,0 +1,12 @@
+package sirjain;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PerfectPlushiesMain implements ModInitializer {
+	public static final String MOD_ID = "perfectplushies";
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Initializing Perfect Plushies!");
+	}
+}
