9b24ca8cea7b1128abd3d44ade8b4e697931a1ec
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-28 16:45:13 -0400

    Add outline shape for plushies

--- src/main/java/sirjain/util/AbstractPlushieBlock.java
+++ src/main/java/sirjain/util/AbstractPlushieBlock.java
@@ -1,10 +1,22 @@
 package sirjain.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 // Add everything you want to occur in all plushies here
 abstract public class AbstractPlushieBlock extends Block {
+	private static final VoxelShape OUTLINE_SHAPE = Block.createCuboidShape(3, 0, 3, 13, 10, 13);
+
 	public AbstractPlushieBlock(Settings settings) {
 		super(settings);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return OUTLINE_SHAPE;
 	}
 }
