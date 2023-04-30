94f04fd5aad0712c19dc06580f8f48e94972ed88
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-29 21:30:47 -0400

    Add dolphin plushie
    Also add missing loot table for dolphin

---
+++ src/main/java/sirjain/plushie_variants/JumpPlushieBlock.java
@@ -0,0 +1,33 @@
+package sirjain.plushie_variants;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.BlockSoundGroup;
+import sirjain.util.AbstractPlushieBlock;
+import sirjain.util.IPlushieBlockSpecialEffects;
+
+public class JumpPlushieBlock extends AbstractPlushieBlock implements IPlushieBlockSpecialEffects {
+	public JumpPlushieBlock() {
+		super(FabricBlockSettings.of(Material.WOOL)
+			.nonOpaque()
+			.sounds(BlockSoundGroup.WOOL)
+			.strength(0.5f, 0.5f)
+			.luminance((state) -> 3)
+		);
+	}
+
+	@Override
+	public ParticleType getParticle() {
+		return ParticleTypes.HEART;
+	}
+
+	@Override
+	public StatusEffectInstance getStatusEffect() {
+		final StatusEffectInstance speedEffect = new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20 * 3, 1);
+		return new StatusEffectInstance(speedEffect);
+	}
+}
--- src/main/java/sirjain/util/PlushieBlockRegistries.java
+++ src/main/java/sirjain/util/PlushieBlockRegistries.java
@@ -15,10 +15,12 @@ public class PlushieBlockRegistries {
 	// Block variables
 	public static Block FOX_PLUSHIE;
 	public static Block DOLPHIN_PLUSHIE;
+	public static Block RABBIT_PLUSHIE;
 
 	// Item variables
 	public static Item FOX_PLUSHIE_BLOCK_ITEM;
 	public static Item DOLPHIN_PLUSHIE_BLOCK_ITEM;
+	public static Item RABBIT_PLUSHIE_BLOCK_ITEM;
 
 	public static void registerPlushieBlocks() {
 		FOX_PLUSHIE = Registry.register(
@@ -30,6 +32,12 @@ public class PlushieBlockRegistries {
 		DOLPHIN_PLUSHIE = Registry.register(
 			Registries.BLOCK,
 			new Identifier(PerfectPlushiesMain.MOD_ID, "dolphin_plushie"),
+			new SwiftPlushieBlock()
+		);
+
+		RABBIT_PLUSHIE = Registry.register(
+			Registries.BLOCK,
+			new Identifier(PerfectPlushiesMain.MOD_ID, "rabbit_plushie"),
 			new SwiftPlushieBlock()
 		);
 	}
@@ -46,12 +54,19 @@ public class PlushieBlockRegistries {
 			new Identifier(PerfectPlushiesMain.MOD_ID, "dolphin_plushie"),
 			new BlockItem(DOLPHIN_PLUSHIE, new Item.Settings())
 		);
+
+		RABBIT_PLUSHIE_BLOCK_ITEM = Registry.register(
+			Registries.ITEM,
+			new Identifier(PerfectPlushiesMain.MOD_ID, "rabbit_plushie"),
+			new BlockItem(RABBIT_PLUSHIE, new Item.Settings())
+		);
 	}
 
 	public static void addItemsToTabs() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
 			entries.add(FOX_PLUSHIE_BLOCK_ITEM);
 			entries.add(DOLPHIN_PLUSHIE_BLOCK_ITEM);
+			entries.add(RABBIT_PLUSHIE_BLOCK_ITEM);
 		});
 	}
 }
