00725ef061515fbc926f70bafb438205d6e16a27
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-05-01 20:32:01 -0400

    Update AbstractPlushieBlock.java

--- src/main/java/sirjain/blocks/util/AbstractPlushieBlock.java
+++ src/main/java/sirjain/blocks/util/AbstractPlushieBlock.java
@@ -1,12 +1,22 @@
 package sirjain.blocks.util;
 
+import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.particle.ParticleEffect;
+import net.minecraft.network.PacketCallbacks;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.scoreboard.AbstractTeam;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -14,10 +24,14 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 abstract public class AbstractPlushieBlock extends Block {
 	private static final VoxelShape OUTLINE_SHAPE = Block.createCuboidShape(3, 0, 3, 13, 10, 13);
-
+	MinecraftClient client;
 	public AbstractPlushieBlock(Settings settings) {
 		super(settings);
 	}
@@ -37,5 +51,12 @@ abstract public class AbstractPlushieBlock extends Block {
 			+ pos.getZ() + (world.random.nextInt(2)),
 			0, 0, 0
 		);
+	}
+
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		MinecraftServer server = world.getServer();
+		server.sendMessage(Text.literal("Hehe!"));
+		return ActionResult.PASS;
 	}
 }
