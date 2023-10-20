c34c1a270d1cbf09b2bd2a5d9d6cb08061c81a9b
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-20 18:09:29 -0400

    Finish goose plushie

--- Fabric/src/main/java/io/github/sirjain0/perfectplushies/PerfectPlushiesClient.java
+++ Fabric/src/main/java/io/github/sirjain0/perfectplushies/PerfectPlushiesClient.java
@@ -4,19 +4,26 @@ import io.github.sirjain0.perfectplushies.client.renderer.PlushieRenderer;
 import io.github.sirjain0.perfectplushies.client.renderer.WanderingPlushieTraderRenderer;
 import io.github.sirjain0.perfectplushies.init.BlockInit;
 import io.github.sirjain0.perfectplushies.init.EntityInit;
+import io.github.sirjain0.perfectplushies.registration.RegistryObject;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.level.block.Block;
 
 public class PerfectPlushiesClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.FROG_PLUSHIE.get(), RenderType.cutout());
+        registerCutout(BlockInit.FROG_PLUSHIE);
+        registerCutout(BlockInit.GOOSE_PLUSHIE);
+
         BlockEntityRenderers.register(BlockInit.PLAYER_PLUSHIE_BLOCK_ENTITY.get(), (context) -> new PlushieRenderer());
         EntityRendererRegistry.register(EntityInit.WANDERING_PLUSHIH_TRADER.get(), WanderingPlushieTraderRenderer::new);
+    }
+
+    private static void registerCutout(RegistryObject<Block> block) {
+        BlockRenderLayerMap.INSTANCE.putBlock(block.get(), RenderType.cutout());
     }
 }
